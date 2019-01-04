ad2f42bfdf754dcbdc78c9cb657d3892622bb507
Ranae Rodgers <ranae.rodgers@icloud.com>
2019-01-04 00:07:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+var kitens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}

68c2c5d0152d27b549abf5f158f073f4d2ceeac6
Ranae Rodgers <ranae.rodgers@icloud.com>
2019-01-04 00:40:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function prependKitten(name) {
 }
 var kittens = ['Milo', 'Otis', 'Garfield'];
 function removeLastKitten() {
-  var newKittens = [...kittens.slice(2,3)];
+  var newKittens = [...kittens.slice(1,2)];
   return newKittens;
 }
